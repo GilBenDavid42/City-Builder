c03562c6d57e5905d66b643bb66dd51291029659
liran777 <liran.zuriel@gmail.com>
2020-07-11 22:34:21 +0300

    Added score
    score system

--- Scripts/GlobalResources.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var civilians = 0
-var money = 1000
--- Scripts/StartMenu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-export(String, FILE, "*.tscn") var next_level
-
-func _on_Start_button_up() -> void:
-	get_tree().change_scene(next_level)
-
-func _on_Exit_button_up() -> void:
-	get_tree().quit()
